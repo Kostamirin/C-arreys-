8e63543a9141374a2842126006ab9f6086d6d6d2
Kostamirin <132436003+Kostamirin@users.noreply.github.com>
2024-01-11 15:26:25 +0300

    The first part.

---
+++ C++(arrays).cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(){
+    std::string car[] = {"Lamborgini", "Porshe", "Lada"};
+    
+    std::cout <<car[0] <<'\n';    
+    std::cout <<car[1] <<'\n';
+    std::cout <<car[2] <<'\n';
+}
